40c4b23b64f88bb7767f15bde0fbd61578496683
Ezequiel Casiano <ezequielcasiano15@gmail.com>
2026-07-12 18:24:21 -0300

    chore: SEO improvements, sitemap, and Open Graph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import remarkBreaks from "remark-breaks";
 
 export default defineConfig({
-    integrations: [mdx()],
+    site: "https://liralatente.vercel.app",
+    integrations: [mdx(), sitemap()],
     markdown: {
         remarkPlugins: [remarkBreaks],
     },
